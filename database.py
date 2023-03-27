4c27b58bcf2a63cb4154993c6bc4f8c1c7fdb388
winfreykong <winfreykxy@gmail.com>
2023-03-27 12:48:01 +0800

    database updated

--- database.py
+++ database.py
@@ -1,1 +1,10 @@
 import sqlite3
+
+conn = sqlite3.connect('user.db')
+
+conn.execute('''CREATE TABLE user(
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                PRIMARY KEY(username);
+            )''')
+
