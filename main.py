f8a21023da3c5579930d06df09031dfd72589735
winfreykong <winfreykxy@gmail.com>
2023-03-30 12:52:40 +0800

    Updated

--- main.py
+++ main.py
@@ -6,45 +6,47 @@ templates = Jinja2Templates(directory='templates/')
 app = FastAPI()
 class Database:
     
-    def __init__(self):
-        self.database = None
+    def __init__(self, path):
+        self.database = sqlite3.connect(path)
+        self.cursor = self.database.cursor()
         
     # Create a function to create a user in the database
-    async def create_user(self, username: str, password: str):
-        async with self.database:
-            query = f"INSERT INTO user (username, password)\
-                VALUES ({username}, {password})"
-            await self.database.execute(query)
-            await self.database.commit()
+    def create_user(self, user: str, pw: str):
+        query = """INSERT INTO user VALUES (?, ?)"""
+        print(query)
+        self.cursor.execute(query, (user, pw))
 
     # Create  some test users
-    async def create_test_users(self):
-        await self.create_user('user1', 'password1')
-        await self.create_user('user2', 'password2')
+    def create_test_users(self):
+        self.create_user('user1', 'password1')
 
-    @app.on_event('startup')
-    async def startup(self):
-        self.database = await sqlite3.connect('user.db')
-        await self.create_test_users()
+path = 'user.db'
 
-    # Define routes
-    @app.get('/')
-    async def read_form(self, request: Request):
-        return templates.TemplateResponse("web.html", {"request": request})
 
-    @app.post('/login')
-    async def login(self, request: Request, username: str = Form(...), password: str = Form(...)):
-        try:
-            async with self.database: 
-                user = await self.database.fetchone(f"SELECT * FROM user WHERE username = '{username}' AND password = '{password}'")
-                
-            if user:
-                return templates.TemplateResponse("success.html", {"request": request, "username": username})
-            else:
-                return templates.TemplateResponse("failure.html", {"request": request})
-        except:
-            return templates.TemplateResponse("error.html", {"request": request})
-        
-    @app.on_event('shutdown')
-    async def shutdown(self):
-        await self.database.close()
+@app.on_event('startup')
+async def startup():
+    global database
+    database = Database(path)
+    database.create_test_users()
+
+# Define routes
+@app.get('/')
+async def read_form(request: Request):
+    return templates.TemplateResponse("web.html", {"request": request})
+
+@app.post('/login')
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    try:
+        async with database: 
+            user = await database.fetchone(f"SELECT * FROM user WHERE username = '{username}' AND password = '{password}'")
+            
+        if user:
+            return templates.TemplateResponse("success.html", {"request": request, "username": username})
+        else:
+            return templates.TemplateResponse("failure.html", {"request": request})
+    except:
+        return templates.TemplateResponse("error.html", {"request": request})
+    
+@app.on_event('shutdown')
+async def shutdown():
+    await database.close()
