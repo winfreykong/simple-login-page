557c78110ac4acabbd810e45857b0d4e218f110e
winfreykong <winfreykxy@gmail.com>
2023-03-31 16:01:12 +0800

    Updated database.py

--- database.py
+++ database.py
@@ -7,10 +7,12 @@ cursor = conn.cursor()
 # Check if table exists
 cursor.execute(''' SELECT name FROM sqlite_master WHERE type='table' AND name='user' ''')
 
+# Clear table if exists
 if cursor.fetchone()[0]==1:
     print('Table exists. Clearing table now..')
     cursor.execute('DELETE FROM students;',)
 
+# Create table if does not exist
 else:
     conn.execute('''CREATE TABLE user(
                 username TEXT NOT NULL,
