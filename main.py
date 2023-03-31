26be71365bdc273c5ed22b5c441187351bf0b26a
winfreykong <winfreykxy@gmail.com>
2023-03-31 15:42:32 +0800

    Updated

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
 from fastapi import FastAPI, Request, Form
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
 import sqlite3
+
 
 templates = Jinja2Templates(directory='templates/')
 app = FastAPI()
 
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"),
+    name="static",
+)
+
 @app.get('/')
-async def read_form(request: Request):
+async def login(request: Request):
     return templates.TemplateResponse("web.html", {"request": request})
 
 @app.post('/login')
 async def login(request: Request, username: str = Form(...), password: str = Form(...)):
     try:
-        async with conn: 
-            user = await conn.fetchone(f"SELECT * FROM user WHERE username = '{username}' AND password = '{password}'")
-            
+        conn = sqlite3.connect("user.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user WHERE username = ? AND password = ?", (username, password))
+        user = cursor.fetchone()
+        conn.close()
+        print(user)
         if user:
             return templates.TemplateResponse("success.html", {"request": request, "username": username})
         else:
             return templates.TemplateResponse("failure.html", {"request": request})
-    except:
+    except Exception:
+        print(Exception)
         return templates.TemplateResponse("error.html", {"request": request})   
-
-
-async def create_user(conn, user: str, pw: str): 
-    query = """INSERT INTO user(username, password) VALUES (?, ?)"""
-    async with conn:
-        conn.execute(query, (user, pw))
-
-async def create_test_users(conn):
-    await create_user(conn, 'user1', 'testingtesttest')
-
-@app.on_event('startup')
-async def startup():
-    global conn
-    conn = sqlite3.connect('user.db')
-    await create_test_users(conn)
-
-
     
-@app.on_event('shutdown')
-async def shutdown():
-    await conn.close()
+@app.get("/register")
+async def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+@app.post("/register")
+async def register(request: Request, username: str = Form(...), password: str = Form(...)):
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO user (username, password) VALUES (?, ?)", (username, password))
+    conn.commit()
+    conn.close()
+
+    return templates.TemplateResponse("success-register.html", {"request": request, "username": username})
